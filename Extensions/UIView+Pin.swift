8eb4cf6c53f13c4da91864427b5a28b4fc58d763
Karen Gabrieli <karengabrieli.viana@outlook.com>
2024-02-26 14:06:55 -0300

    Video 3 - Add TableView

---
+++ Extensions/UIView+Pin.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Pin.swift
+//  TravelPlanningApp
+//
+//  Created by Karen Oliveira on 24/02/24.
+//
+
+import UIKit
+
+extension UIView {
+
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+    }
+}
